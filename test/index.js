98b6590ae20eea855755b5a330815ca9d7478d84
ihollander <ianholla@gmail.com>
2019-02-05 08:12:13 -0500

    Done.

--- test/index.js
+++ test/index.js
@@ -1,33 +1,30 @@
-var expect = require('chai').expect
-var fs = require('fs')
-var path = require('path')
-var expectedNodeVersion = process.version
-var expectedPlatform = ['darwin', 'linux']
-var expectedEnv = process.env.NODE_ENV
+var expect = require("chai").expect;
+var fs = require("fs");
+var path = require("path");
+var expectedNodeVersion = process.version;
+var expectedPlatform = ["darwin", "linux"];
+var expectedEnv = process.env.NODE_ENV;
 
-describe('node', function () {
-  it('must have version ' + expectedNodeVersion, function(done){
-    var actualNodeVersion = require(path.join(__dirname, '../version'))
-    expect(actualNodeVersion).to.be.equal(expectedNodeVersion)
-    done()
-  })
-})
+describe("node", function() {
+  it("must have version " + expectedNodeVersion, function(done) {
+    var actualNodeVersion = require(path.join(__dirname, "../version"));
+    expect(actualNodeVersion).to.be.equal(expectedNodeVersion);
+    done();
+  });
+});
 
-describe('platform', function () {
-  it('must be ' + expectedPlatform, function(done){
-    var actualPlatform = require(path.join(__dirname, '../platform'))
-    expect(actualPlatform).to.be.oneOf(expectedPlatform)
-    done()
-  })
-})
+describe("platform", function() {
+  it("must be " + expectedPlatform, function(done) {
+    var actualPlatform = require(path.join(__dirname, "../platform"));
+    expect(actualPlatform).to.be.oneOf(expectedPlatform);
+    done();
+  });
+});
 
-
-describe('env var NODE_ENV', function () {
-  it('must be ' + expectedEnv, function(done){
-    var actualEnv = require(path.join(__dirname, '../envvar'))
-    expect(actualEnv).to.equal(expectedEnv)
-    done()
-  })
-})
-
-
+describe("env var NODE_ENV", function() {
+  it("must be " + expectedEnv, function(done) {
+    var actualEnv = require(path.join(__dirname, "../envvar"));
+    expect(actualEnv).to.equal(expectedEnv);
+    done();
+  });
+});
